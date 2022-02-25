b5f93aaf30ebed0206cf8dff25a9eec4c97553be
Kawan Melo Fernandes <kawanmelofernandes@gmail.com>
2022-02-25 20:26:36 -0300

    working

--- soma.java
+++ soma.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class soma {
 
     public static void main(String[]args){
-        Scanner entrada = new Scanner(System.in);
-
-        System.out.println("Digite um número:");
-        int n1 = entrada.nextInt();
-        System.out.println("Digite um número:");
-        int n2 = entrada.nextInt();
-         
-        int soma = n1+n2;
-        
-        System.out.println("A soma de " + n1+  " mais " + n2 +" é igual a " + soma);
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Digite um número:");
+            int n1 = entrada.nextInt();
+            System.out.println("Digite um número:");
+            int n2 = entrada.nextInt();
+             
+            int soma = n1+n2;
+            
+            System.out.println("A soma de " + n1+  " mais " + n2 +" é igual a " + soma);
+        }
     }
 }
